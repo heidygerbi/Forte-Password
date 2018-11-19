5e7dd58851493cf11bdcd013aeedc89b8990b183
heidygerbi <heidyjoanagr@gmail.com>
2018-11-19 12:31:38 -0500

    cifrar y descifrar frases completas en index js, toma en consideracion espacios sin desplazamiento

--- src/index.js
+++ src/index.js
@@ -7,25 +7,51 @@
 		const fraseMayuscula=fraseInicial.toUpperCase();
 		//alert(`Frase mayuscula: ${fraseMayuscula}`);
 		//saber el desplazamiento
-		const desplazamiento= document.getElementById('desplazamiento').value;
+		const desplazamiento=document.getElementById('desplazamiento').value;
 		//alert(`desplazamiento: ${desplazamiento}`);
 		//saber tamaño de la frase
 		const largoFrase= fraseInicial.length;
 		//alert(`largo de la frase ${largoFrase}`);
 		//recorrer letra por letra 
-		for (let i=0; i<= fraseMayuscula.length-1; i++) {
-			alert (`${i} : ${fraseMayuscula.charCodeAt(i)}`);//transforma en ANCII
-			const resultado=((((fraseMayuscula.charCodeAt(i)-65)+desplazamiento)%26)+65);
-			alert (`el nuevo codigo ASCII es: ${resultado} `); 
+		for (let i=0; i< largoFrase; i++) {
+			let fraseANCIIDesplazado="";
+			const fraseANCII=fraseMayuscula.charCodeAt(i);//transforma en ANCII
+			if(fraseANCII[i]!=32){
+				fraseANCIIDesplazado=((((fraseANCII-65)+parseInt(desplazamiento))%26)+65);
+			}else if(fraseANCII[i]==32){
+				fraseANCIIDesplazado=fraseANCII;
+			}
+			const fraseCifrada=String.fromCharCode(fraseANCIIDesplazado); 
 		}
-
-
+//		return fraseCifrada;
 	}
 	function descifrarFrase(){
+		//saber la frase
 		const fraseInicial= document.getElementById('frase').value;
-		alert(`${fraseInicial}`);
-
+		//alert(`Frase inicial: ${fraseInicial}`);
+		//CONVERTIR FRASE EN MAYUSCULA
+		const fraseMayuscula=fraseInicial.toUpperCase();
+		//alert(`Frase mayuscula: ${fraseMayuscula}`);
+		//saber el desplazamiento
+		const desplazamiento=document.getElementById('desplazamiento').value;
+		//alert(`desplazamiento: ${desplazamiento}`);
+		//saber tamaño de la frase
+		const largoFrase= fraseInicial.length;
+		//alert(`largo de la frase ${largoFrase}`);
+		//recorrer letra por letra 
+		for (let i=0; i< largoFrase; i++) {
+			let fraseANCIIDesplazado="";
+			const fraseANCII=fraseMayuscula.charCodeAt(i);//transforma en ANCII
+			if(fraseANCII[i]!=32){
+				fraseANCIIDesplazado=((((fraseANCII-65)-parseInt(desplazamiento))%26)+65);
+			}else if(fraseANCII[i]==32){
+				fraseANCIIDesplazado=fraseANCII;
+			}
+			//const fraseANCIIDesplazado=((((fraseANCII-65)-parseInt(desplazamiento))%26)+65);
+			const fraseDesifrada=String.fromCharCode(fraseANCIIDesplazado); 
+		}
+//		return fraseDesifrada;
 	}	
-
+	resultado.innerHTML = cifrarFrase();
 	document.getElementById('cifrar').addEventListener('click', cifrarFrase);	
 	document.getElementById('descifrar').addEventListener('click', descifrarFrase);	
