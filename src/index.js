6e966bc5c926880a2fd269beed9fe9272daf606d
Heidy Gerbi <heidyjoanagr@gmail.com>
2018-11-20 17:47:20 -0500

    cifrado y descifrado en index.js funcinales :)

--- src/index.js
+++ src/index.js
@@ -18,13 +18,11 @@ const btnCifrar = document.getElementById('btn-cifrar');
 		let fraseANCIIDesplazado=[];
 		let fraseANCII=[];
 		let fraseCifrada="";
-		//recorrer letra por letra 
 		for (let i=0; i<fraseMayuscula.length; i++) {
 			fraseANCII[i]=parseInt(fraseMayuscula.charCodeAt(i));//transforma en ANCII
 			if(fraseANCII[i]!==32 && fraseANCII[i]>=65 && fraseANCII[i]<=90)fraseANCIIDesplazado[i]=(((fraseANCII[i]-65)+desplazamiento)%26)+65;
 			fraseCifrada+=String.fromCharCode(fraseANCIIDesplazado[i]); 
 		}
-		//console.log(fraseCifrada);
 		return fraseCifrada;
 	}
 //funcion para descifrar
@@ -33,24 +31,13 @@ const descifrarFrase= (fraseInicial,desplazamiento)=>{
 	let fraseANCIIDesplazado=[];
 	let fraseANCII=[];
 	let fraseDescifrada="";
-	//recorrer letra por letra 
 	for (let i=0; i<fraseMayuscula.length; i++) {
 		fraseANCII[i]=parseInt(fraseMayuscula.charCodeAt(i));//transforma en ANCII
-		/*if(fraseANCII[i]!==32 && fraseANCII[i]>=65 && fraseANCII[i]<=90){
-			let a=(fraseANCII[i]-65);
-			console.log(a);
-			let b=(a-desplazamiento);
-			console.log(b);
-			let c=(b%26);
-			console.log(c);
-			let d=c+65;
-			console.log(d);
-			fraseANCIIDesplazado[i]=d;
-		}*/
-		if(fraseANCII[i]!==32 && fraseANCII[i]>=65 && fraseANCII[i]<=90)fraseANCIIDesplazado[i]=(((fraseANCII[i]-65)+desplazamiento)%26)+65;
+ 		if(fraseANCII[i]!==32 && fraseANCII[i]>=65 && fraseANCII[i]<=90){
+			fraseANCIIDesplazado[i]=(((fraseANCII[i]-90)-desplazamiento)%26)+90;
+		}
 		fraseDescifrada+=String.fromCharCode(fraseANCIIDesplazado[i]); 
 
 	}
-	//console.log(fraseDescifrada);
 	return fraseDescifrada;
 }
