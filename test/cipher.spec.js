290bf449ea4eedd44ccf133a5ceef9225a083294
Heidy Gerbi <heidyjoanagr@gmail.com>
2018-11-29 20:30:45 -0500

    test nuevos

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -13,6 +13,12 @@ describe('cipher', () => {
     it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33', ()=>{
       assert.equal(cipher.encode(33,"ABCDEFGHIJKLMNOPQRSTUVWXYZ"),'HIJKLMNOPQRSTUVWXYZABCDEFG');
     });
+    it('debería retornar "234" para "123" con offest 1', ()=>{
+      assert.equal(cipher.encode(1,"123"),'234');
+    });
+    it('debería retornar "BcD" para "ABC" con offest 1 y dificultad = si', ()=>{
+      assert.equal(cipher.encode(1,"ABC",null,null,"si"),'BcD');
+    });
   });
 
   describe('cipher.decode', () => {
@@ -23,6 +29,9 @@ describe('cipher', () => {
 
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33',()=>{
       assert.equal(cipher.decode(33,"HIJKLMNOPQRSTUVWXYZABCDEFG"),'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    });
+    it('debería retornar "123" para "234" con offest 1',()=>{
+      assert.equal(cipher.decode(1,"234"),'123');
     });
   });
 
